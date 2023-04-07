a44af87bc0804b6810fcf70989191f5397573021
Elo-G <121858614+Elo-G@users.noreply.github.com>
2023-04-07 17:19:52 +0200

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,5 +17,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2.5 seconds
+  setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 }
